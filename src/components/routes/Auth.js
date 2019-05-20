472c5c2dfd8944498dfeac40c0597877480f3325
EricB <ericblancas23@gmail.com>
2019-05-20 15:16:16 -0700

    creating calendar and google api

--- src/components/routes/Auth.js
+++ src/components/routes/Auth.js
@@ -4,34 +4,34 @@ export default class Auth extends Component {
   render() {
     return (
       <React.Fragment>
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                <div className="card card-signin my-5">
-                    <div className="card-body bg-danger">
-                        <h5 className="card-title text-center text-dark">Sign In</h5>
-                        <form className="form-signin">
-                        <div className="form-label-group">
-                          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-                          <label for="inputEmail">Email address</label>
-                        </div>
+        <div className="container">
+        <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+            <div className="card-body bg-danger">
+                <h5 className="card-title text-center text-dark">Sign In</h5>
+                <form className="form-signin">
+                <div className="form-label-group">
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                    <label for="inputEmail">Email address</label>
+                </div>
 
-                        <div className="form-label-group">
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                        <label for="inputPassword">Password</label>
-                        </div>
+                <div className="form-label-group">
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                <label for="inputPassword">Password</label>
+                </div>
 
-                        <div className="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                        <label className="custom-control-label text-dark" for="customCheck1">Remember password</label>
-                        </div>
+                <div className="custom-control custom-checkbox mb-3">
+                <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+                <label className="custom-control-label text-dark" for="customCheck1">Remember password</label>
+                </div>
 
-                        <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                        <hr class="my-4" />
-                        <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-                        <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
-                        </form>
-                    </div>
+                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                <hr class="my-4" />
+                <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
+                <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                </form>
+                </div>
                 </div>
               </div>
             </div>
