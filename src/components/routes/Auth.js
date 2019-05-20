547e7319a99d0c9789c6b797fd24e49d2cfccd89
EricB <ericblancas23@gmail.com>
2019-05-20 01:36:46 -0700

    fixed styling

--- src/components/routes/Auth.js
+++ src/components/routes/Auth.js
@@ -8,8 +8,8 @@ export default class Auth extends Component {
             <div className="row">
               <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div className="card card-signin my-5">
-                    <div className="card-body">
-                        <h5 className="card-title text-center text-danger">Sign In</h5>
+                    <div className="card-body bg-danger">
+                        <h5 className="card-title text-center text-dark">Sign In</h5>
                         <form className="form-signin">
                         <div className="form-label-group">
                           <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
@@ -23,7 +23,7 @@ export default class Auth extends Component {
 
                         <div className="custom-control custom-checkbox mb-3">
                         <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                        <label className="custom-control-label text-danger" for="customCheck1">Remember password</label>
+                        <label className="custom-control-label text-dark" for="customCheck1">Remember password</label>
                         </div>
 
                         <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
