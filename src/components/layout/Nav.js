00d1d4c946e475b50c81dc2ee54b6a954e0939ab
EricB <ericblancas23@gmail.com>
2019-05-22 01:09:11 -0700

    fixed styling

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,21 +4,21 @@ import '../../App.css';
 
 export const Nav = () => (
     <React.Fragment>
-        <ul className="nav text-danger">
-            <li className="nav-list">
-                <Link to='/' className="nav-link text-danger">Home</Link>
+        <ul className="nav">
+            <li className="nav-list" style={{marginRight: "0.5cm", marginLeft: "0.5cm"}}>
+                <Link to='/' className="nav-link btn btn-outline-danger text-light">Home</Link>
             </li>
-            <li className="nav-list">
-                <Link to='/programs' className="nav-link text-danger">Programs</Link>
+            <li className="nav-list" style={{marginRight: "0.5cm"}}>
+                <Link to='/programs' className="nav-link btn btn-outline-danger text-light">Programs</Link>
             </li>
-            <li className="nav-list">
-                <Link to='/schedule' className="nav-link text-danger">Schedule</Link>
+            <li className="nav-list" style={{marginRight: "0.5cm"}}>
+                <Link to='/schedule' className="nav-link btn btn-outline-danger text-light">Schedule</Link>
             </li>
-            <li className="nav-list">
-                <Link to='/about' className="nav-link text-danger">About</Link>
+            <li className="nav-list" style={{marginRight: "0.5cm"}}>
+                <Link to='/about' className="nav-link btn btn-outline-danger text-light">About</Link>
             </li>
-            <li className="nav-list">
-                <Link to='/contact' className="nav-link text-danger">Contact</Link>
+            <li className="nav-list" style={{marginRight: "0.5cm"}}>
+                <Link to='/contact' className="nav-link btn btn-outline-danger text-light">Contact</Link>
             </li>
         </ul>
         <hr />
--- src/components/routes/Programs.js
+++ src/components/routes/Programs.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import '../../App.css'
 
 export default class Programs extends Component {
   render() {
     return (
-      <div>
-        Programs
-      </div>
+      <React.Fragment style={{ flex: "row"}}>
+        <div className="card text-danger" style={{width: "35rem"}}>
+            <div className="card-box">
+                <div className="card-header">
+                <h5 className="card-title">Under 18 Karate Class</h5>
+                <a className="btn btn-dark" href="https://www.youtube.com/watch?v=IcGDS93A0i8">Click here</a> for a class demonstration
+                </div>
+                <p className="card-text float-left">Come have your child participate in the top karate school in valley!</p>
+                <img src={require('../assets/under-18-karate.jpg')} alt="uner18karate" className="card-img-top"/>
+            </div>
+        </div>
+        <div className="card text-danger" style={{width: "35rem"}}>
+            <div className="card-box">
+                <div className="card-header">
+                <h5 className="card-title">Under 18 Karate Class</h5>
+                <a className="btn btn-dark" href="https://www.youtube.com/watch?v=IcGDS93A0i8">Click here</a> for a class demonstration
+                </div>
+                <p className="card-text float-left">Come have your child participate in the top karate school in valley!</p>
+                <img src={require('../assets/under-18-karate.jpg')} alt="uner18karate" className="card-img-top"/>
+            </div>
+        </div>
+      </React.Fragment>
     )
   }
 }
